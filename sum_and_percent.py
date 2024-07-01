4d392b62cbd6534bb5cf9d91989a07b4d7778655
nandinii-2 <marguttikarnandini@gmail.com>
2024-07-01 19:15:06 +0530

    sum_and_percent.py

---
+++ sum_and_percent.py
@@ -0,0 +1,8 @@
+a=int(input("subject 1 "))
+b=int(input("subject 2"))
+c=int(input("subject 3"))
+d=int(input("subject 4"))
+e=int(input("subject 5"))
+total_marks = a + b + c + d + e
+percentage = (total_marks / 5)
+print("Final percentage:", percentage)
